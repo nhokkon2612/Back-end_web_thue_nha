de4c3f9c8ab898bc67f44a09dedf2415891adf9f
nhokkon2612 <ngoctranxz@gmail.com>
2021-11-03 15:34:17 +0700

    Tao middleware jwt, logout, me

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -11,7 +11,6 @@ class AuthController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login']]);
     }
 
     public function register(Request $request) {
--- routes/api.php
+++ routes/api.php
@@ -15,16 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group([
-
-    'middleware' => 'api',
+Route::group(['middleware' => ['api'],
     'prefix' => 'auth'
-
 ], function ($router) {
 
-    Route::post('login', [AuthController::class,'login']);
-    Route::post('register', [AuthController::class, 'register']);
-//    Route::post('refresh', 'AuthController@refresh');
-//    Route::post('me', 'AuthController@me');
+    Route::middleware('jwt.auth')->group(function () {
+        Route::get('me', [AuthController::class, 'me']);
+        Route::get('logout',[AuthController::class,'logout']);
+    });
 
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('refresh', [AuthController::class,'refresh']);
 });
